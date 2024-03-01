b15ea1575ed6649cb4452503e26214b1532d018a
uzairzia <uzair.governorsindh@gmail.com>
2024-03-01 08:28:41 +0500

    initial commit

---
+++ Guest_list.js
@@ -0,0 +1,6 @@
+"use strict";
+let guest_list = ['Faizan Ali', 'Umair Khan', 'Abdul Ghaffar'];
+let my_name = 'Uzair Zia';
+for (let i = 0; i < guest_list.length; i++) {
+    console.log('\nDear Mr. ' + guest_list[i] + ',\nIts my pleasure to invite you for the dinner. \nRegards, \n' + my_name);
+}
---
+++ Guest_list.ts
@@ -0,0 +1,8 @@
+let guest_list: string[]= ['Faizan Ali', 'Umair Khan', 'Abdul Ghaffar'];
+let my_name: string = 'Uzair Zia';
+
+for(let i=0; i<guest_list.length; i++){
+
+console.log ('\nDear Mr. '+guest_list[i]+ ',\nIts my pleasure to invite you for the dinner. \nRegards, \n' +my_name );
+
+}
